6fa8b4f4e291b9b767ca8958319a705cf83e15b3
joel8019 <joel8019@yahoo.com>
2022-03-14 18:47:10 -0500

    Fully implement directory enumeration

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import requests
 
 
 def main():
-
-    ans = input("What would you like to do today? \n(0) Search for subdomains \n(1) Directory Enumeration \n(2) Both\n")
-    print(ans)
-    subdomains = open("file.txt").read().splitlines()
-    domain = input("Please enter the domain name ")
+    ans = int(input("What would you like to do today? \n(0) Search for subdomains \n(1) Directory Enumeration \n(2) "
+                    "Both\n"))
+    domain = ""
+    # Subdomain Enumeration
     if ans == 0 or ans == 2:
+        subdomains = open("subdomain.txt").read().splitlines()
+        domain = input("Please enter the domain name ")
         for s in subdomains:
             url = f"http://{s}.{domain}"
             try:
@@ -18,8 +19,17 @@ def main():
                 print(s)
             except requests.ConnectionError:
                 pass
-    if ans == 1 or ans ==2:
-
+    # Directory Enumeration
+    if ans == 1 or ans == 2:
+        directories = open("directory.txt").read().splitlines()
+        if ans == 1:
+            domain = input("Please enter the domain name ")
+        for d in directories:
+            url = f"http://{domain}/{d}.html"
+            request = requests.get(url)
+            # If HTML response code is less than 400
+            if request.ok:
+                print(f"{d} request.status_code")
 
 
 if __name__ == "__main__":
